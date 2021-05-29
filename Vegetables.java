91e2aee3a6568482af8396062117e5e870b02021
Yuri Goncharov <ygpanzer@gmail.com>
2021-05-29 16:36:41 +0200

    Create Vegetables.java

---
+++ Vegetables.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Vegetables {
+
+    String Name;
+    String color;
+    String taste;
+    int size;
+    int weight;
+    int price;
+
+    public Vegetables(String Name){
+
+        this.Name = Name;
+
+    }
+
+}
