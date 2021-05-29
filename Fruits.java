526171c02fcbe2dc831bfa3d93df0a93d9bb0eaf
Yuri Goncharov <ygpanzer@gmail.com>
2021-05-29 16:36:15 +0200

    Create Fruits.java

---
+++ Fruits.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Fruits {
+
+    String Name;
+    String color;
+    String taste;
+    int size;
+    int weight;
+    int price;
+
+    public Fruits(String Name){
+
+        this.Name = Name;
+
+    }
+
+}
